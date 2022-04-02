23ae9844ce2df861084856434cfd72e30fc4638a
voodoohop <t.haferlach@gmail.com>
2022-04-02 22:38:12 +0200

    Update main.py

--- lucidsonicdreams/main.py
+++ lucidsonicdreams/main.py
@@ -28,10 +28,6 @@ if not os.path.exists('stylegan2'):
 
 #StyleGAN2 Imports
 cwd = os.getcwd()
-os.chdir('stylegan2')
-import dnnlib
-from dnnlib.tflib.tfutil import * 
-os.chdir(cwd)
 
 
 def show_styles():
