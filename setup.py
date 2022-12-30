4409a6b1a922147ddaa906d5ed6adf483ba48e26
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-12-29 15:40:46 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("./content/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
 
 setuptools.setup(
-    name="lucidsonicdreams", 
-    version="0.4",
-    author="Alain Mikael Alafriz",
-    author_email="mikaelalafriz@gmail.com",
+    name="lucidsonicdreams v2", 
+    version="0.01",
+    author="BigZaddy",
+    author_email="@gmail.com",
     description="Syncs GAN-generated visuals to music",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mikaelalafriz/lucid-sonic-dreams",
-    download_url="https://github.com/mikaelalafriz/lucid-sonic-dreams/archive/v_04.tar.gz",
+    python_requires='>=3.7',
+    url="https://github.com/DeFi-Coder-News-Letter/lucid-sonic-dreams",
+    download_url="https://github.com/DeFi-Coder-News-Letter/lucid-sonic-dreams/lucid-sonic-dreams.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['tensorflow==1.15',
+    install_requires=['tensorflow==1.15.0',
                       'librosa',
                       'numpy',
                       'moviepy',
